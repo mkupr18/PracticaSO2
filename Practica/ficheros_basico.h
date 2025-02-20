aac8b42fbf574fa3e9e27e27532a86864ef88ae4
SpicyCat101 <aizkalyarat@gmail.com>
2025-02-20 09:51:25 +0100

    nada importante

--- Practica/ficheros_basico.h
+++ Practica/ficheros_basico.h
@@ -3,9 +3,18 @@
 // Constantes
 #define posSB = 0
 #define tamSB = 1
+//#define tamMB = 13
+//#define tamAI = 3125
+// El tamaño total de metadatos = la suma de los tamaños = 3139
 #define INODOSIZE = 1024   // bytes
 
+// Inicialización de las funciones
+int tamAI(unsigned int ninodos);
+int tamMB(unsigned int nbloques);
 int initSB(unsigned int nbloques, unsigned int ninodos);
+int initMB(); 
+int initAI();
+
 
 // 4 bytes cada uno y tenemos 12 -> 4*12 = 48 bytes
 struct superbloque {
@@ -24,32 +33,32 @@ struct superbloque {
     char padding[BLOCKSIZE - 12 * sizeof(unsigned int)]; // Relleno para ocupar el bloque completo
  };
 
- struct inodo {     // comprobar que ocupa 128 bytes haciendo un sizeof(inodo)!!!
-    unsigned char tipo;     // Tipo ('l':libre, 'd':directorio o 'f':fichero)
-    unsigned char permisos; // Permisos (lectura y/o escritura y/o ejecución)
- 
+ struct inodo {              // comprobar que ocupa 128 bytes haciendo un sizeof(inodo)!!!
+    unsigned char tipo;      // Tipo ('l':libre, 'd':directorio o 'f':fichero)
+    unsigned char permisos;  // Permisos (lectura y/o escritura y/o ejecución)
+                             // Solo tendremos en cuenta los permisos del user (rwx: 000 - 111)
  
     /* Por cuestiones internas de alineación de estructuras, si se está utilizando
-     un tamaño de palabra de 4 bytes (microprocesadores de 32 bits):
+    un tamaño de palabra de 4 bytes (microprocesadores de 32 bits):
     unsigned char reservado_alineacion1 [2];
     en caso de que la palabra utilizada sea del tamaño de 8 bytes
     (microprocesadores de 64 bits): unsigned char reservado_alineacion1 [6]; */
     unsigned char reservado_alineacion1[6];
  
  
-    time_t atime; // Fecha y hora del último acceso a datos
-    time_t mtime; // Fecha y hora de la última modificación de datos
-    time_t ctime; // Fecha y hora de la última modificación del inodo
-    time_t btime; // Fecha y hora de creación del inodo (birth)
+    time_t atime;   // Fecha y hora del último acceso a datos
+    time_t mtime;   // Fecha y hora de la última modificación de datos
+    time_t ctime;   // Fecha y hora de la última modificación del inodo
+    time_t btime;   // Fecha y hora de creación del inodo (birth)
  
  
     /* comprobar que el tamaño del tipo time_t para vuestra plataforma/compilador es 8:
     printf ("sizeof time_t is: %ld\n", sizeof(time_t)); */
  
  
-    unsigned int nlinks;             // Cantidad de enlaces de entradas en directorio
-    unsigned int tamEnBytesLog;      // Tamaño en bytes lógicos (EOF)
-    unsigned int numBloquesOcupados; // Cantidad de bloques ocupados zona de datos
+    unsigned int nlinks;                // Cantidad de enlaces de entradas en directorio
+    unsigned int tamEnBytesLog;         // Tamaño en bytes lógicos (EOF)
+    unsigned int numBloquesOcupados;    // Cantidad de bloques ocupados zona de datos
  
  
     unsigned int punterosDirectos[12];  // 12 punteros a bloques directos
