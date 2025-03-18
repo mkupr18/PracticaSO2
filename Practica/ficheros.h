c4d769ea8640075ff9db3c3c57b2ed55dfd96756
Rupakguni <parlatguni@gmail.com>
2025-03-18 21:03:02 +0100

    Es

---
+++ Practica/ficheros.h
@@ -0,0 +1,21 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct STAT {
+    char tipo;
+    unsigned char permisos;
+    unsigned int nlinks;
+    unsigned int tamEnBytesLog;
+    time_t atime;
+    time_t mtime;
+    time_t ctime;
+    unsigned int numBloquesOcupados;
+};
+
+// Funciones de la capa de ficheros
+int mi_write_f(unsigned int ninodo, const void *buf_original, unsigned int offset, unsigned int nbytes);
+int mi_read_f(unsigned int ninodo, void *buf_original, unsigned int offset, unsigned int nbytes);
+int mi_stat_f(unsigned int ninodo, struct STAT *p_stat);
+int mi_chmod_f(unsigned int ninodo, unsigned char permisos);
