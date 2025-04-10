64b6c156622e95eecf62cec1272657bd89c91713
SpicyCat101 <aizkalyarat@gmail.com>
2025-04-10 11:11:16 +0200

    nivel 7

--- Practica/leer_sf.c
+++ Practica/leer_sf.c
@@ -5,7 +5,20 @@
 #include "directorios.h"
 #define DEBUGN1 0
 #define DEBUGN2 0
-#define DEBUGN7 0
+#define DEBUGN7 1
+
+void mostrar_buscar_entrada(char *camino, char reservar){
+    unsigned int p_inodo_dir = 0;
+    unsigned int p_inodo = 0;
+    unsigned int p_entrada = 0;
+    int error;
+    printf("\ncamino: %s, reservar: %d\n", camino, reservar);
+    if ((error = buscar_entrada(camino, &p_inodo_dir, &p_inodo, &p_entrada, reservar, 6)) < 0) {
+      mostrar_error_buscar_entrada(error);
+    }
+    printf("**********************************************************************\n");
+    return;
+}
 
 int main(int argc, char **argv)
 {
